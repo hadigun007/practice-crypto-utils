1223e6e9544dd068f0482ddf08fe2abf5ae72ae3
hadi <hadi@depository.id>
2023-11-01 09:10:26 +0700

    add wallet log

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const wallet = require("./wallet.js")
 
-let r = wallet.generate()
-
-console.log("Address: \t"+r.getAddressString())
-console.log("Private key: \t"+r.getPrivateKeyString())
-console.log("Public key: \t"+r.getPublicKeyString())
+wallet.generate()
--- wallet.js
+++ wallet.js
@@ -1,9 +1,24 @@
-const Wallet = require('ethereumjs-wallet');
+const Wallet = require("ethereumjs-wallet");
+const fs = require("fs");
 
+const generate = () => {
+  let w = Wallet.default.generate();
+  let data = {
+    hex:{
+        address: w.getAddressString(),
+        public_key: w.getPublicKeyString(),
+        private_key: w.getPrivateKeyString(),
+    },
+    buffer:{
+        address: w.getAddress(),
+        address_checksum : w.getChecksumAddressString(),
+        public_key: w.getPublicKey(),
+        private_key: w.getPrivateKey(),
+    },
+  }
+  let name = Math.random(99999).toString().replace('0.', '')
+  fs.writeFileSync(`wallet/wallet-${name}.json`, JSON.stringify(data));
+  return w;
+};
 
-const generate = ()=>{
-    return Wallet.default.generate()
-}
-
-
-module.exports = {generate} 
+module.exports = { generate };
